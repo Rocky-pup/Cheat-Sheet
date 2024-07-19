9cd5f3cba33bd4314c600ca4f3e82f76b9a2eeee
Rocky <rocky_pup@tuta.io>
2024-07-19 10:10:45 +0000

    Extend what I need to setup here

--- coder-setup.sh
+++ coder-setup.sh
@@ -21,3 +21,28 @@ nvm install v20
 
 # Install global npm packages
 npm i -g yarn typescript tsx pm2 npm@latest
+
+# Update packages cache
+sudo apt-get update
+
+# Upgrade packages
+sudo apt-get upgrade
+
+# Install postgresql
+sudo apt install postgresql postgresql-contrib
+
+# Start psql without systemd cause it's coder docker env
+sudo service postgresql start
+
+# Install nano cause I like it
+sudo apt-get install nano
+
+# Install redis
+sudo apt-get install redis
+
+# Start redis
+sudo service redis-server start
+
+# Setup one-time local database for testing 
+sudo -u postgres psql -c "CREATE USER alpha WITH LOGIN PASSWORD 'SomeSecureAlpha';"
+sudo -u postgres psql -c "CREATE DATABASE alpha OWNER alpha;" 
