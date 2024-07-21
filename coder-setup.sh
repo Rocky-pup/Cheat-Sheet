6f6f356d1b73db87285d3de9898c76b99d7353b0
Rocky <rocky_pup@tuta.io>
2024-07-21 08:40:21 +0000

    yes

--- coder-setup.sh
+++ coder-setup.sh
@@ -28,17 +28,11 @@ sudo apt-get update -y
 # Upgrade packages
 sudo apt-get upgrade -y
 
-# Install postgresql
-sudo apt-get install postgresql postgresql-contrib -y
+# Install things
+sudo apt-get install postgresql postgresql-contrib nano redis -y
 
 # Start psql without systemd cause it's coder docker env
 sudo service postgresql start
-
-# Install nano cause I like it
-sudo apt-get install nano -y
-
-# Install redis
-sudo apt-get install redis -y
 
 # Start redis
 sudo service redis-server start
